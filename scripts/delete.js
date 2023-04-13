592375e3380c50b872df2468948ba42fa5d7d536
CaiooGomes <gomescaio898@gmail.com>
2023-04-13 17:16:49 -0300

    delete ok

--- scripts/delete.js
+++ scripts/delete.js
@@ -1,3 +1,15 @@
+const urldelete = "https://crud-javascript-caio-default-rtdb.firebaseio.com/pessoas/"
+
 function deleteData(id){
-    console.log(id)
+    fetch(`${urldelete}${id}.json`,{
+        method: 'DELETE',
+        headers:{
+            'content-Type': 'application/json'
+        }
+    })
+    .then(res=>{
+        PegarDados();
+    })
+    .catch(err=>console.error("Erro ao deletar dado",err))
+
 }
