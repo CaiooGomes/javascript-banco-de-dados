4c3dc4bf1c5a1ddba10b656c8852270c4a8e39b5
CaiooGomes <gomescaio898@gmail.com>
2023-04-20 16:38:58 -0300

    arrumando bug

--- scripts/update.js
+++ scripts/update.js
@@ -2,8 +2,10 @@
 //PATCH MODIFICA APENAS OS ATRIBUTOS ENVIADOS
 //PUT MODIFICA O OBJETO INTEIRO
 
-var editMode = document.getElementById("submit").innerHTML = "Edit Aqui!"
+var editMode = document.getElementById("submit").innerHTML === "Edit Aqui!"
 
 function EnviandoDadosAtuais(id,nameitem,emailitem){
-    
+    document.getElementById("email").value = emailitem    
+    document.getElementById("name").value = emailitem    
 }
+
