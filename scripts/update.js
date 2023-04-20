2b464cb223827041973ecb31feaeefde25840543
CaiooGomes <gomescaio898@gmail.com>
2023-04-20 17:27:09 -0300

    editar ok

--- scripts/update.js
+++ scripts/update.js
@@ -4,9 +4,9 @@
 
 var editMode = document.getElementById("submit").innerHTML === "Edit Aqui!"
 
-function EnviandoDadosAtuais(id,nameitem,emailitem){
+function EnviandoDadosAtuais(id,emailitem,nameitem){
     document.getElementById("email").value = emailitem;    
-    document.getElementById("name").value = emailitem;
+    document.getElementById("name").value = nameitem;
     
     document.getElementById("submit").setAttribute("data-id",id);
     document.getElementById("submit").innerHTML = "Edit Aqui!"
@@ -24,21 +24,21 @@ async function editData(id){
         name: name,
         email: email,
     }
-    Response = await fetch(`${urlEdit}${id}.json`),{
+    response = await fetch(`${urlEdit}${id}.json`,{
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
-    }
+    } )
     .then(res=>{
-        PegarDados();
-        document.getElementById('submit').removeAttribute('data-id');
-        document.getElementById("email").value='';
-        document.getElementById("name").value='';
-        document.getElementById("submit").innerText='Cadastrar!';
-        editMode= document.getElementById('submit').innerHTML === "Edit Aqui!";
+            PegarDados();
+            document.getElementById('submit').removeAttribute('data-id');
+            document.getElementById("email").value='';
+            document.getElementById("name").value='';
+            document.getElementById("submit").innerText='Cadastrar!';
+            editMode= document.getElementById('submit').innerHTML === "Edit Aqui!";
         })
-        
-        .catch(err=>console.log("erro ao editar",err));
+    .catch(err=>console.log("erro ao editar",err))
+   
 }
