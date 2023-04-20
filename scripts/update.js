45f43a18037c99cdb4c13acdc09cfff779e8703d
CaiooGomes <gomescaio898@gmail.com>
2023-04-20 16:45:24 -0300

    função de atualizar o form ok]

--- scripts/update.js
+++ scripts/update.js
@@ -5,7 +5,13 @@
 var editMode = document.getElementById("submit").innerHTML === "Edit Aqui!"
 
 function EnviandoDadosAtuais(id,nameitem,emailitem){
-    document.getElementById("email").value = emailitem    
-    document.getElementById("name").value = emailitem    
+    document.getElementById("email").value = emailitem;    
+    document.getElementById("name").value = emailitem;
+    
+    document.getElementById("submit").setAttribute("data-id",id);
+    document.getElementById("submit").innerHTML = "Edit Aqui!"
+    editMode = document.getElementById("submit").innerHTML === "Edit Aqui!"
+    // console.log(editMode)
+    
 }
 
